387a0d7217a885b4e7862a449e05193e33ab4df9
Justin Grunwald <Justin.Grunwald@mymhc.ca>
2023-09-22 15:19:22 -0600

    Got tests inserted and UI setup and class library setup.

--- PROG-210-Assignment-Test/UnitTest1.cs
+++ PROG-210-Assignment-Test/UnitTest1.cs
@@ -1,11 +1,17 @@
+using TaxClassLibrary;
+
 namespace PROG_210_Assignment_Test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalcFedTax_2500()
         {
+            FedTax tax = new FedTax();
+            decimal result = tax.federaltaxcalc(2500);
+            Assert.AreEqual(375, result);
+
         }
     }
 }
--- PROG-210-Group-Assignment/Form1.cs
+++ PROG-210-Group-Assignment/Form1.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography.X509Certificates;
 
+using TaxClassLibrary;
+
 namespace PROG_210_Group_Assignment
 {
     public partial class Form1 : Form
@@ -14,7 +16,13 @@ namespace PROG_210_Group_Assignment
 
         private void bttnSubmit_Click(object sender, EventArgs e)
         {
+            //Figure out which province is selected, and call the proper method.
+
             income = decimal.Parse(bttnSubmit.Text);
+            FedTax  tax= new FedTax();
+            decimal fedTax = tax.federaltaxcalc(income);
+            ProvTax provTax = new ProvTax();
+            decimal abTax = provTax.abTax(income);
         }
     }
 }
