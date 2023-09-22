4539f7ee253aeee1af6ec70d2def96db1e232f28
Justin Grunwald <Justin.Grunwald@mymhc.ca>
2023-09-22 14:09:23 -0600

    Add project files.

---
+++ PROG-210-Assignment-Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PROG_210_Assignment_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PROG-210-Group-Assignment/Form1.cs
@@ -0,0 +1,10 @@
+namespace PROG_210_Group_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
