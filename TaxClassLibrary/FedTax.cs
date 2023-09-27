fa81490e465f2c41a9ef88daceccbba1b52b6212
Codey Mitchell <Codey.Mitchell@mymhc.ca>
2023-09-27 14:37:06 -0600

    updated code for FedTax, still need to check return and some final lines of code

--- TaxClassLibrary/FedTax.cs
+++ TaxClassLibrary/FedTax.cs
@@ -19,7 +19,7 @@ namespace TaxClassLibrary
 
             decimal totalUntaxed = income;
 
-            //15%	on the portion of taxable income that is $53,359 or less, plus
+            //15% on the portion of taxable income that is $53,359 or less, plus
             if (income < fedIncome[0])
             {
                 tax = federalTax[0] * income;
@@ -44,7 +44,45 @@ namespace TaxClassLibrary
                 tax += federalTax[1] * fedIncome[1];
                 totalTaxed += tax;
             }
+            
+            //26% on the portion of taxable income over $106,717 up to $165,430, plus
+            if (income >= fedIncome[1] && income <= fedIncome[2])
+            {
+                totalUntaxed -= totalTaxed;
+                tax += totalUntaxed * federalTax[2];
+                return income = tax;
+            }
+            if (income > fedIncome[2])
+            {
+                tax += federalTax[2] * fedIncome[2];
+                totalTaxed += tax;
+            }
 
+            //29% on the portion of taxable income over $165,430 up to $235,675, plus
+            if (income >= fedIncome[2] && income <= fedIncome[3])
+            {
+                totalUntaxed -= totalTaxed;
+                tax += totalUntaxed * federalTax[3];
+                return income = tax;
+            }
+            if (income > fedIncome[3])
+            {
+                tax += federalTax[3] * fedIncome[3];
+                totalTaxed += tax;
+            }
+
+            //33% on the portion of taxable income over $235,675
+            if (income >= fedIncome[3] && income <= fedIncome[4])
+            {
+                totalUntaxed -= totalTaxed;
+                tax += totalUntaxed * federalTax[4];
+                return income = tax;
+            }
+            if (income > fedIncome[4])
+            {
+                tax += federalTax[4] * fedIncome[4];
+                totalTaxed += tax;
+            }
             return income;
         }
 
