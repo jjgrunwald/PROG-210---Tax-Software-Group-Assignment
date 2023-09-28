9acda46686f2a3c8adf09d0d49143239dc77634e
jjgrunwald <113119782+jjgrunwald@users.noreply.github.com>
2023-09-27 21:57:53 -0600

    I have added all of the tax information to comments in the methods for the provincial tax. I have also added a seperate method that is passed the information from the provincial methods.

--- TaxClassLibrary/ProvTax.cs
+++ TaxClassLibrary/ProvTax.cs
@@ -8,18 +8,91 @@ namespace TaxClassLibrary
 
         //Alberta Tax
 
-        public decimal abTax(decimal income)
+        public void abTax(decimal income)
         {
+                                /*            Tax rate    Taxable income threshold
+                                10 % on the portion of taxable income that is $142,292 or less, plus
+                                12 % on the portion of taxable income over $142,292 up to $170,751, plus
+                                13 % on the portion of taxable income over $170,751 up to $227,668, plus
+                                14 % on the portion of taxable income over $227,668 up to $341,502, plus
+                                15 % on the portion of taxable income over $341,502*/
+
             decimal[] provincialTax = { 0.10m, 0.12m, 0.13m, 0.14m, 0.15m };
             decimal[] incomeBracket = { 142292m, 170751m, 227668m, 341502m };
 
+            TaxCalculator(income, provincialTax, incomeBracket);
+        }
+
+        public void bcTax(decimal income)
+        {
+                        /*            Tax rate    Taxable income threshold
+                        5.06 % on the portion of taxable income that is $45,654 or less, plus
+                        7.7 % on the portion of taxable income over $45,654 up to $91,310, plus
+                        10.5 % on the portion of taxable income over $91,310 up to $104,835, plus
+                        12.29 % on the portion of taxable income over $104,835 up to $127,299, plus
+                        14.7 % on the portion of taxable income over $127,299 up to $172,602, plus
+                        16.8 % on the portion of taxable income over $172,602 up to $240,716, plus
+                        20.5 % on the portion of taxable income over $240,716*/
+
+            decimal[] provincialTax = { 0.0506m, 0.0707m, 0.1005m, 0.1229m, 0.147m, 0.168m, 0.205m };
+            decimal[] incomeBracket = { 45654m, 91310m, 104835m, 127299m, 172602m, 240716m};
+
+            TaxCalculator(income, provincialTax, incomeBracket);
+
+        }
+
+        public void skTax(decimal income)
+        {
+/*                                    Tax rate    Taxable income threshold
+                        10.5 % on the portion of taxable income that is $49,720 or less, plus
+                        12.5 % on the portion of taxable income over $49,720 up to $142,058, plus
+                        14.5 % on the portion of taxable income over $142,058*/
+
+            decimal[] provincialTax = { 0.0506m, 0.0707m, 0.1005m, 0.1229m, 0.147m, 0.168m, 0.205m };
+            decimal[] incomeBracket = { 45654m, 91310m, 104835m, 127299m, 172602m, 240716m };
+
+            TaxCalculator(income, provincialTax, incomeBracket);
+        }
+
+        public void nwtTax(decimal income)
+        {
+                            /*            Tax rate Taxable income threshold
+                            5.9 % on the portion of taxable income that is $48,326 or less, plus
+                            8.6 % on the portion of taxable income over $48,326 up to $96,655, plus
+                            12.2 % on the portion of taxable income over $96,655 up to $157,139, plus
+                            14.05 % on the portion of taxable income over $157,139*/
+        }
+
+        public void onTax(decimal income)
+        {
+                            /*            Tax rate    Taxable income threshold
+                            5.05 % on the portion of taxable income that is $49,231 or less, plus
+                            9.15 % on the portion of taxable income over $49,231 up to $98,463, plus
+                            11.16 % on the portion of taxable income over $98,463 up to $150,000, plus
+                            12.16 % on the portion of taxable income over $150,000 up to $220,000, plus
+                            13.16 % on the portion of taxable income over $220,000*/
+        }
+
+        public void nsTax(decimal income)
+        {
+                                /*            Tax rate Taxable income threshold
+                                8.79 % on the portion of taxable income that is $29,590 or less, plus
+                                14.95 % on the portion of taxable income over $29,590 up to $59,180, plus
+                                16.67 % on the portion of taxable income over $59,180 up to $93,000, plus
+                                17.5 % on the portion of taxable income over $93,000 up to $150,000, plus
+                                21 % on the portion of taxable income over $150,000*/
+        }
+
+            //Tax Calculator Method
+
+        public decimal TaxCalculator(decimal income, decimal[] provincialTax, decimal[] incomeBracket)
+        {
             decimal tax = 0;
 
             decimal totalTaxed = 0;
 
             decimal totalUntaxed = income;
 
-            //10% on the portion of taxable income that is $142,292 or less, plus
             if (income < incomeBracket[0])
             {
                 tax = provincialTax[0] * income;
@@ -30,7 +103,6 @@ namespace TaxClassLibrary
                 tax += provincialTax[0] * incomeBracket[0];
                 totalTaxed += tax;
             }
-
 
             for (int i = 0; i < incomeBracket.Length; i++)
             {
@@ -57,178 +129,9 @@ namespace TaxClassLibrary
                 return tax;
             }
             else
-            return tax;
-
-
-            //attempting to refactor all of this code below, into the loop above.
-            /*
-                        decimal[] abTax = { 0.10m, 0.12m, 0.13m, 0.14m, 0.15m };
-                        decimal[] abIncome = { 142292m, 170751m, 227668m, 341502m };
-
-                        decimal tax = 0;
-
-                        decimal totalTaxed = 0;
-
-                        decimal totalUntaxed = income;
-
-                        //10% on the portion of taxable income that is $142,292 or less, plus
-                        if (income < abIncome[0])
-                        {
-                            tax = abTax[0] * income;
-                            totalTaxed += tax;
-                        }
-                        if (income > abIncome[0])
-                        {
-                            tax += abTax[0] * abIncome[0];
-                            totalTaxed += tax;
-                        }
-
-                        for (int i = 0; i < abIncome.Length; i++)
-                        {
-                            if (income >= abIncome[i] && income <= abIncome[i]+1)
-                            {
-                                totalUntaxed -= totalTaxed;
-                                tax += totalUntaxed * abTax[i]+1;
-                                return income = tax;
-                            }
-                            if (income > abIncome[i]+1)
-                            {
-                                tax += abTax[i]+1 * abIncome[i]+1;
-                                totalTaxed += tax;
-                            }
-
-                        }
-
-                        return income + tax;
-
-                        //12% on the portion of taxable income over $142,292 up to $170,751, plus
-                        if (income >= abIncome[0] && income <= abIncome[1])
-                        {
-                            totalUntaxed -= totalTaxed;
-                            tax += totalUntaxed * abTax[1];
-                            return income = tax;
-                        }
-                        if (income > abIncome[1])
-                        {
-                            tax += abTax[1] * abIncome[1];
-                            totalTaxed += tax;
-                        }
-
-                        //13% on the portion of taxable income over $170,751 up to $227,668, plus
-                        if (income >= abIncome[1] && income <= abIncome[2])
-                        {
-                            totalUntaxed -= totalTaxed;
-                            tax += totalUntaxed * abTax[2];
-                            return income = tax ;
-                        }
-                        if (income > abIncome[2])
-                        {
-                            tax += abTax[2] * abIncome[2];
-                            totalTaxed += tax;
-                        }
-
-                        //14% on the portion of taxable income over $227,668 up to $341,502, plus
-                        if (income >= abIncome[2] && income <= abIncome[3])
-                        {
-                            totalUntaxed -= totalTaxed;
-                            tax += totalUntaxed * abTax[3];
-                            return income = tax;
-                        }
-                        if (income > abIncome[3])
-                        {
-                            tax += abTax[3] * abIncome[3];
-                            totalTaxed += tax;
-                        }
-
-                        //15% on the portion of taxable income over $341,502
-                        if (income >= abIncome[3] && income <= abIncome[4])
-                        {
-                            totalUntaxed -= totalTaxed;
-                            tax += totalUntaxed * abTax[4];
-                            return income = tax;
-                        }
-                        if (income > abIncome[3])
-                        {
-                            tax += abTax[3] * income;
-                            totalTaxed += tax;
-                        }
-
-                        return income + tax;*/
+         return tax;
 
         }
-
-        public decimal bcTax(decimal income)
-        {
-            /*            5.06 % on the portion of taxable income that is $45,654 or less, plus
-            7.7 % on the portion of taxable income over $45,654 up to $91,310, plus
-            10.5 % on the portion of taxable income over $91,310 up to $104,835, plus
-            12.29 % on the portion of taxable income over $104,835 up to $127,299, plus
-            14.7 % on the portion of taxable income over $127,299 up to $172,602, plus
-            16.8 % on the portion of taxable income over $172,602 up to $240,716, plus
-            20.5 % on the portion of taxable income over $240,716*/
-
-
-            decimal[] provincialTax = { 0.0506m, 0.0707m, 0.1005m, 0.1229m, 0.147m, 0.168m, 0.205m };
-            decimal[] incomeBracket = { 45654m, 91310m, 104835m, 127299m, 172602m, 240716m};
-
-            decimal tax = 0;
-
-            decimal totalTaxed = 0;
-
-            decimal totalUntaxed = income;
-
-            //10% on the portion of taxable income that is $142,292 or less, plus
-            if (income < incomeBracket[0])
-            {
-                tax = provincialTax[0] * income;
-                totalTaxed += tax;
-            }
-            if (income > incomeBracket[0])
-            {
-                tax += provincialTax[0] * incomeBracket[0];
-                totalTaxed += tax;
-            }
-
-            for (int i = 0; i < incomeBracket.Length; i++)
-            {
-                if (income >= incomeBracket[i] && income <= incomeBracket[i] + 1)
-                {
-                    totalUntaxed -= totalTaxed;
-                    tax += totalUntaxed * provincialTax[i] + 1;
-                    return income = tax;
-                }
-                if (income > incomeBracket[i] + 1)
-                {
-                    tax += provincialTax[i] + 1 * incomeBracket[i] + 1;
-                    totalTaxed += tax;
-                }
-
-            }
-
-            return income + tax;
-
-        }
-
-        public decimal skTax(decimal income)
-        {
-            return 0;
-        }
-
-        public decimal nwtTax(decimal income)
-        {
-            return 0;
-        }
-
-        public decimal onTax(decimal income)
-        {
-            return 0;
-        }
-
-        public decimal nsTax(decimal income)
-        {
-            return 0;
-        }
-
 
     }
 }
